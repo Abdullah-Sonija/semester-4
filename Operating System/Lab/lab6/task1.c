ae7f70a47fd1af20b38d7129899cbd4ac545bacd
Abdullah_Sonija <abdullahsonija28@gmail.com>
2026-03-22 02:28:21 +0500

    Update task1.c

--- Operating System/Lab/lab6/task1.c
+++ Operating System/Lab/lab6/task1.c
@@ -11,10 +11,8 @@ int main() {
     int pid;
     int fd;
     
-    // Modification 1 & 3: Change buffer size to 50 and set message to "Hello Child"
     char message[50] = "Hello Child"; 
 
-    // Create the FIFO (named pipe)
     mkfifo(FIFO_NAME, 0666);
 
     pid = fork();
@@ -23,24 +21,19 @@ int main() {
         printf("Fork Failed\n");
         return 1;
     } else if (pid == 0) {
-        // --- Child Process ---
+        
         fd = open(FIFO_NAME, O_RDONLY);
         read(fd, message, sizeof(message));
         
-        // Modification 2: Print the message two times
         printf("Child received (1st time): %s\n", message);
         printf("Child received (2nd time): %s\n", message);
         
-        // Modification 4: Print the total number of characters in the message
-        // We use %lu because strlen returns a size_t type.
         printf("Total characters in message: %lu\n", strlen(message));
         
         close(fd);
     } else {
-        // --- Parent Process ---
         fd = open(FIFO_NAME, O_WRONLY);
         
-        // Parent sends the message without taking input from the user
         write(fd, message, strlen(message) + 1);
         close(fd);
         
