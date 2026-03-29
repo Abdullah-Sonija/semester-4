1ef8da6be604343e2c5e2ea69adf3eef7b224102
Abdullah_Sonija <abdullahsonija28@gmail.com>
2026-03-29 17:02:26 +0500

    Create q1.sql

---
+++ Database System/Lab/lab5/q1.sql
@@ -0,0 +1,5 @@
+select count(*) as Employees_count from Employees;
+select max (salary) from Employees;
+select min (salary) from Employees;
+select sum(salarY) from Employees;
+select count(*) as Employees_count from Employees group by DEPARTMENT_ID;
